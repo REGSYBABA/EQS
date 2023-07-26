405471b4ae1bd2b05fab74c3ab3069b452d999e5
Omoregha Michael <Regsybaba@Reghas-MacBook-Air.local>
2023-07-26 19:16:14 +0100

    This is the assignment from eloquent javascript 4 teaching about objects

---
+++ EQS4/script.js
@@ -0,0 +1,34 @@
+let arraay = [1,2,3]
+console.log(arraay.length)
+
+function reverseArray(array){
+     let val = []
+      for (let i = array.length - 1; i >= 0; i--){
+        val.push(array[i])
+      }
+      return val
+ }
+  
+  
+console.log(reverseArray(["A", "B", "C"]));
+
+let arrayValue = [1, 2, 3, 4, 5];
+
+function reverseArrayInPlace(array){
+     for (let i = 0; i < Math.floor(array.length / 2); i++){
+        let poop = array[i]
+        array[i] = array[array.length - 1 -i]
+        array[array.length - 1 -i] = poop
+      }
+      return array
+}
+
+
+
+
+
+
+
+reverseArrayInPlace(arrayValue);  
+
+console.log(arrayValue)
